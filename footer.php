ddf115794686ed232ccd512265949279346eb7b6
pmpr.dev <pmpr.dev@gmail.com>
2025-08-21 14:15:37 +0000

    Obfuscated

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             68a7249f45d12             |
+    |             68a729d0c48a8             |
     |_______________________________________|
 */
  pmpr_do_action('render_footer');
--- src/Pmpr/Cover/Filefox/Container.php
+++ src/Pmpr/Cover/Filefox/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             68a7249f45d12             |
+    |             68a729d0c48a8             |
     |_______________________________________|
 */
  namespace Pmpr\Cover\Filefox; use Pmpr\Common\Foundation\Container\Container as BaseClass; use Pmpr\Cover\Filefox\Traits\CommonTrait; abstract class Container extends BaseClass { use CommonTrait; }
