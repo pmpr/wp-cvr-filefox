74d70e1a2255b583aa23c8b748496a9eb7cc741f
pmpr.dev <pmpr.dev@gmail.com>
2025-08-25 15:48:29 +0000

    Obfuscated

--- src/Pmpr/Cover/Filefox/Woocommerce/Common.php
+++ src/Pmpr/Cover/Filefox/Woocommerce/Common.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             68a729d0c48a8             |
+    |             68ac858fded43             |
     |_______________________________________|
 */
  namespace Pmpr\Cover\Filefox\Woocommerce; use Pmpr\Cover\Filefox\Container; abstract class Common extends Container { }
